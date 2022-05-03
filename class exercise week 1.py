6c270a19ebbecdb43e5e443f7a3ea44151fe652f
harshukv <69118352+harshukv@users.noreply.github.com>
2022-05-03 19:39:03 -0400

    Add files via upload

---
+++ class exercise week 1.py
@@ -0,0 +1,15 @@
+
+a=int(input('Enter a numerical value'))
+b=int(input('Enter second numerical value'))
+x=a+b
+print(x)
+x=a-b
+print(x)
+x=a*b
+print(x)
+x=a/b
+print(x)
+x=a**b
+print(x)
+x=a%b
+print(x)
